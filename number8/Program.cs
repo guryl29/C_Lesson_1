f0bf0cf0215cca6783cd58307a54716949af704f
guryl29 <111236089+guryl29@users.noreply.github.com>
2022-08-22 23:40:14 +0300

    gitignore

---
+++ number8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+string numberStr;
+numberStr=Console.ReadLine();
+int n = int.Parse(numberStr);
+int a = 1;
+while (a<=n)
+{
+    if ("a%2==0")
+    {
+        Console.WriteLine(a);
+    }
+    a++;
+}
+
+
+
