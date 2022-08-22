908c9cc22198fd4188fe1c855c5d089281e7a7cb
guryl29 <111236089+guryl29@users.noreply.github.com>
2022-08-22 23:30:11 +0300

    Number_2

---
+++ number2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input number b: ");
+int b = int.Parse(Console.ReadLine());
+
+
+if (a > b)
+{
+     Console.WriteLine(" число a = max, число b = min");
+}
+else
+{
+    Console.WriteLine("число a = min, число b = max");
+}
