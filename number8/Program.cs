22d4902d8115b157df720681774c2d5e40c33534
guryl29 <111236089+guryl29@users.noreply.github.com>
2022-08-23 14:41:30 +0300

    Number_8

--- number8/Program.cs
+++ number8/Program.cs
@@ -1,16 +1,11 @@
 ﻿Console.WriteLine("Введите число: ");
-string numberStr;
-numberStr=Console.ReadLine();
-int n = int.Parse(numberStr);
-int a = 1;
-while (a<=n)
+int n = int.Parse(Console.ReadLine());
+for (int a = 1; a <= n; a++)
 {
-    if ("a%2==0")
+    if (a%2==0)
     {
         Console.WriteLine(a);
     }
-    a++;
 }
-
 
 
